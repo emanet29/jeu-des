d16d01b42c2cec43b5394be457bb35a9e81ed8f9
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 15:47:50 +0200

    last commit

--- Jeu de dés/ViewController.swift
+++ Jeu de dés/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var de: UIImageView!
+    @IBOutlet weak var dede: UIImageView!
+    
+    var isShaking = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if !isShaking {
+            if motion == .motionShake {
+                self.isShaking = true
+                var timer = Timer()
+                var nombreDeTours = 0
+                timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true, block: { (t) in
+                    if nombreDeTours > 10 {
+                        timer.invalidate()
+                        self.isShaking = false
+                    } else {
+                        self.de.image = self.imageAleatoire()
+                        self.dede.image = self.imageAleatoire()
+                        nombreDeTours += 1
+                    }
+                })
+            }
+        }
     }
-
+    
+    func imageAleatoire() -> UIImage{
+        let aleatoire = Int(arc4random_uniform(6)) + 1
+        let aleatoireString = String(aleatoire)
+        if let img = UIImage(named: aleatoireString) {
+            return img
+        } else {
+            return UIImage(named: "1")!
+        }
+    }
 
 }
 
